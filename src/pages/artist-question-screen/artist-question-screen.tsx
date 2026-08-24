d81b45b30621cfa8da0f339a4e78ea825e79a784
Ramil <zarram89@gmail.com>
2026-08-24 12:10:14 +0500

    5.8. Уберёт подключение AudioPlayer из GenreQuestionScreen. WIP Компонент `AudioPlayer` больше не нужен в `GenreQuestionScreen`. Для этого мы создали HOC. Следовательно, подключение компонента необходимо удалить, а заодно убрать из стейта `activePlayer`.

--- src/pages/artist-question-screen/artist-question-screen.tsx
+++ src/pages/artist-question-screen/artist-question-screen.tsx
@@ -1,22 +1,24 @@
-import {ChangeEvent} from 'react';
+import {useState, FormEvent, ChangeEvent} from 'react';
 import {Helmet} from 'react-helmet-async';
 import Logo from '../../components/logo/logo';
-import {QuestionArtist, UserArtistQuestionAnswer} from '../../types/question';
+import {QuestionGenre, UserGenreQuestionAnswer} from '../../types/question';
 
-type ArtistQuestionScreenProps = {
-  question: QuestionArtist;
-  onAnswer: (question: QuestionArtist, answer: UserArtistQuestionAnswer) => void;
+type GenreQuestionScreenProps = {
+  question: QuestionGenre;
+  onAnswer: (question: QuestionGenre, answers: UserGenreQuestionAnswer) => void;
   renderPlayer: (src: string, playerIndex: number) => JSX.Element;
 };
 
-function ArtistQuestionScreen(props: ArtistQuestionScreenProps): JSX.Element {
+function GenreQuestionScreen(props: GenreQuestionScreenProps): JSX.Element {
   const {question, onAnswer, renderPlayer} = props;
-  const {answers, song} = question;
+  const {answers, genre} = question;
+
+  const [userAnswers, setUserAnswers] = useState([false, false, false, false]);
 
   return (
-    <section className="game game--artist">
+    <section className="game game--genre">
       <Helmet>
-        <title>Угадай мелодию. Кто исполняет эту песню?</title>
+        <title>Угадай мелодию. Выберите треки</title>
       </Helmet>
       <header className="game__header">
         <Logo />
@@ -28,44 +30,46 @@ function ArtistQuestionScreen(props: ArtistQuestionScreenProps): JSX.Element {
         </svg>
 
         <div className="game__mistakes">
-          <div className="wrong" />
-          <div className="wrong" />
-          <div className="wrong" />
+          <div className="wrong"/>
+          <div className="wrong"/>
+          <div className="wrong"/>
         </div>
       </header>
 
       <section className="game__screen">
-        <h2 className="game__title">Кто исполняет эту песню?</h2>
-        <div className="game__track">
-          <div className="track">
-            {renderPlayer(song.src, 0)}
-          </div>
-        </div>
+        <h2 className="game__title">Выберите {genre} треки</h2>
+        <form
+          className="game__tracks"
+          onSubmit={(evt: FormEvent<HTMLFormElement>) => {
+            evt.preventDefault();
+            onAnswer(question, userAnswers);
+          }}
+        >
+          {answers.map((answer, id) => {
+            const keyValue = `${id}-${answer.src}`;
+            return (
+              <div key={keyValue} className="track">
+                {renderPlayer(answer.src, id)}
+                <div className="game__answer">
+                  <input className="game__input visually-hidden" type="checkbox" name="answer" value={`answer-${id}`}
+                    id={`answer-${id}`}
+                    checked={userAnswers[id]}
+                    onChange={({target}: ChangeEvent<HTMLInputElement>) => {
+                      const value = target.checked;
+                      setUserAnswers([...userAnswers.slice(0, id), value, ...userAnswers.slice(id + 1)]);
+                    }}
+                  />
+                  <label className="game__check" htmlFor={`answer-${id}`}>Отметить</label>
+                </div>
+              </div>
+            );
+          })}
 
-        <form className="game__artist">
-          {answers.map((answer, id) => (
-            <div key={answer.artist} className="artist">
-              <input
-                className="artist__input visually-hidden"
-                type="radio"
-                name="answer"
-                value={`answer-${id}`}
-                id={`answer-${id}`}
-                onChange={(evt: ChangeEvent<HTMLInputElement>) => {
-                  evt.preventDefault();
-                  onAnswer(question, answer.artist);
-                }}
-              />
-              <label className="artist__name" htmlFor={`answer-${id}`}>
-                <img className="artist__picture" src={answer.picture} alt={answer.artist} />
-                {answer.artist}
-              </label>
-            </div>
-          ))}
+          <button className="game__submit button" type="submit">Ответить</button>
         </form>
       </section>
     </section>
   );
 }
 
-export default ArtistQuestionScreen;
+export default GenreQuestionScreen;
