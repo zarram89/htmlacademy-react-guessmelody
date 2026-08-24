8d21a08fb9d155f392ac72839442bf6afdbdf1d4
Ramil <zarram89@gmail.com>
2026-08-24 11:33:27 +0500

    5.4. Подключит компонент AudioPlayer в GenreQuestionScreen. Аналогичную процедуру проделаем в компоненте `GenreQuestionScreen`.

--- src/pages/genre-question-screen/genre-question-screen.tsx
+++ src/pages/genre-question-screen/genre-question-screen.tsx
@@ -1,6 +1,7 @@
 import {useState, FormEvent, ChangeEvent} from 'react';
 import {Helmet} from 'react-helmet-async';
 import Logo from '../../components/logo/logo';
+import AudioPlayer from '../../components/audio-player/audio-player';
 import {QuestionGenre, UserGenreQuestionAnswer} from '../../types/question';
 
 type GenreQuestionScreenProps = {
@@ -48,7 +49,8 @@ function GenreQuestionScreen(props: GenreQuestionScreenProps): JSX.Element {
               <div key={keyValue} className="track">
                 <button className="track__button track__button--play" type="button"/>
                 <div className="track__status">
-                  <audio
+                  <AudioPlayer
+                    autoPlay={id === 0}
                     src={answer.src}
                   />
                 </div>
