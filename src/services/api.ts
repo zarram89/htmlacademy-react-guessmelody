aea60ca53b8f0410c04927d4818ce7a1a7ca88cd
Ramil <zarram89@gmail.com>
2026-08-26 14:58:34 +0500

    7.14. Добавит обработку ошибок взаимодействия с сервером С помощью интерсептора мы добавляем обработку ответов от сервера. В случае, если мы получаем соответствующую ошибку, мы вызываем функцию `processErrorHandle` и передаем ей сообщение ошибки. Таким образом, мы связали все элементы системы обработки ошибок ответа от сервера.

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,22 @@
-import axios, {AxiosInstance, AxiosRequestConfig} from 'axios';
+import axios, {AxiosInstance, AxiosRequestConfig, AxiosResponse, AxiosError} from 'axios';
+import {StatusCodes} from 'http-status-codes';
 import {getToken} from './token';
+import {processErrorHandle} from './process-error-handle';
 
-const BACKEND_URL = 'https://10.react.htmlacademy.pro/guess-melody';
+type DetailMessageType = {
+  type: string;
+  message: string;
+}
+
+const StatusCodeMapping: Record<number, boolean> = {
+  [StatusCodes.BAD_REQUEST]: true,
+  [StatusCodes.UNAUTHORIZED]: true,
+  [StatusCodes.NOT_FOUND]: true
+};
+
+const shouldDisplayError = (response: AxiosResponse) => !!StatusCodeMapping[response.status];
+
+const BACKEND_URL = 'https://13.design.pages.academy/guess-melody';
 const REQUEST_TIMEOUT = 5000;
 
 export const createAPI = (): AxiosInstance => {
@@ -9,7 +24,6 @@ export const createAPI = (): AxiosInstance => {
     baseURL: BACKEND_URL,
     timeout: REQUEST_TIMEOUT,
   });
-
 
   api.interceptors.request.use(
     (config: AxiosRequestConfig) => {
@@ -21,6 +35,19 @@ export const createAPI = (): AxiosInstance => {
 
       return config;
     },
+  );
+
+  api.interceptors.response.use(
+    (response) => response,
+    (error: AxiosError<DetailMessageType>) => {
+      if (error.response && shouldDisplayError(error.response)) {
+        const detailMessage = (error.response.data);
+
+        processErrorHandle(detailMessage.message);
+      }
+
+      throw error;
+    }
   );
 
   return api;
